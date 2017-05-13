9762b4ae0accac36c57979bc134a430246607deb
Edwin Jow <jow.edwin@gmail.com>
2017-05-13 00:11:14 -0400

    Testing server

--- react-ui/src/components/Admin.js
+++ react-ui/src/components/Admin.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Admin extends Component {
     componentDidMount(){
+        axios.get('/test', function(err, response){
+            if(err) throw err;
+            console.log(response);
+        });
     }
     render() {
         return (
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +13,13 @@ app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 app.get('/api', function (req, res) {
     res.set('Content-Type', 'application/json');
     res.send('{"message":"Hello from the custom server!"}');
+});
+
+app.get('/test', function(req, res){
+    MongoClient.connect(process.env.MONGODB_URI, function(err, dbs){
+        if(err) throw err;
+        res.send('{"message": "test"}')
+    })
 });
 
 // All remaining requests return the React app, so it can handle routing.
