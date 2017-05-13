c8b63c63914d86f870b508aff850b653c6c0db0e
Edwin Jow <jow.edwin@gmail.com>
2017-05-12 23:55:33 -0400

    Test

--- react-ui/src/components/RealEstateTracker.js
+++ react-ui/src/components/RealEstateTracker.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 class RealEstateTracker extends Component {
     componentDidMount(){
-        const env = runtimeEnv();
-        console.log(env)
+        console.log(process.env)
     }
     render() {
         return (
