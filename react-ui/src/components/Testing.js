a8239bfdd1dd6f3aba6ac5990f43b8f24590565b
Edwin Jow <jow.edwin@gmail.com>
2018-05-22 21:44:35 -0400

    testing react-vis

--- react-ui/src/components/Testing.js
+++ react-ui/src/components/Testing.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+// import '../../node_modules/react-vis/dist/style.css';
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, VerticalGridLines} from 'react-vis';
 
 class Testing extends Component {
     constructor(props) {
@@ -10,22 +12,35 @@ class Testing extends Component {
     }
 
     componentDidMount() {
-        axios.get('/census')
-            .then(res => {
-                debugger;
-                this.setState({ pop: res.data });
-            });
+        // axios.get('/census')
+        //     .then(res => {
+        //         debugger;
+        //         this.setState({ pop: res.data });
+        //     });
     }
 
     render() {
+        const data = [
+            {x: 0, y: 8},
+            {x: 1, y: 5},
+            {x: 2, y: 4},
+            {x: 3, y: 9},
+            {x: 4, y: 1},
+            {x: 5, y: 7},
+            {x: 6, y: 6},
+            {x: 7, y: 3},
+            {x: 8, y: 2},
+            {x: 9, y: 0}
+          ];
         return (
-            <div>
-                <h1>BEPPPPPPPPPPPPPPPPPPPPPPPPPP</h1>
-                <div>
-                    <ul>
-                        {this.state.pop}
-                    </ul>
-                </div>
+            <div className="App">
+                <XYPlot height={300} width={300}>
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <XAxis />
+                    <YAxis />
+                    <LineSeries data={data} />
+                </XYPlot>
             </div>
         );
     }
